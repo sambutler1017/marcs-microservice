aef493a3125331c7b133f89ffff26dd18bb62051
Samuel Butler <sambutler1017@icloud.com>
2022-03-25 13:16:48 -0400

    no message

--- src/main/java/com/marcs/websockets/client/WebSocketClient.java
+++ src/main/java/com/marcs/websockets/client/WebSocketClient.java
@@ -2,6 +2,7 @@ package com.marcs.websockets.client;
 
 import com.marcs.annotations.interfaces.Client;
 import com.marcs.app.notifications.client.domain.Notification;
+import com.marcs.websockets.rest.WebSocketController;
 import com.marcs.websockets.service.WebSocketService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,19 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 @Client
 public class WebSocketClient {
-    @Autowired
-    private WebSocketService service;
 
-    public void sendWebNotification(Notification message) throws Exception {
-        service.sendWebNotification(message);
+    @Autowired
+    private WebSocketController controller;
+
+    /**
+     * Method that will send a web notification to the desired user. If the receiver
+     * ID is null then it will not send it to the user and will only be sent to the
+     * admins of the webnsite.
+     * 
+     * @param notification The notification to be sent.
+     * @throws Exception
+     */
+    public void sendWebNotification(Notification notification) throws Exception {
+        controller.sendWebNotification(notification);
     }
 }
--- src/main/java/com/marcs/websockets/service/WebSocketService.java
+++ src/main/java/com/marcs/websockets/service/WebSocketService.java
@@ -2,7 +2,12 @@ package com.marcs.websockets.service;
 
 import static com.marcs.websockets.client.domain.WebSocketGlobals.NOTIFICATIONS;
 
+import com.google.common.collect.Sets;
 import com.marcs.app.notifications.client.domain.Notification;
+import com.marcs.app.user.client.UserProfileClient;
+import com.marcs.app.user.client.domain.User;
+import com.marcs.app.user.client.domain.request.UserGetRequest;
+import com.marcs.common.enums.WebRole;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -16,10 +21,55 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class WebSocketService {
+
     @Autowired
     private SimpMessagingTemplate template;
 
-    public void sendWebNotification(Notification message) throws Exception {
-        template.convertAndSend(NOTIFICATIONS, message);
+    @Autowired
+    private UserProfileClient userProfileClient;
+
+    /**
+     * Method that will send a web notification to the desired user. If the receiver
+     * ID is null then it will not send it to the user and will only be sent to the
+     * admins of the webnsite.
+     * 
+     * @param notification The notification to be sent.
+     * @throws Exception
+     */
+    public void sendWebNotification(Notification notification) throws Exception {
+        Integer receiver = notification.getReceiverId() == 0 ? null : notification.getReceiverId();
+
+        if (receiver != null) {
+            sendNotificationToUser(notification, receiver);
+        }
+        sendNotificationToAdmins(notification);
+    }
+
+    /**
+     * Method that will send the notification to all ADMINS and SITE_ADMINS on the
+     * website.
+     * 
+     * @param notification The notification to be sent.
+     * @throws Exception
+     */
+    private void sendNotificationToAdmins(Notification notification) throws Exception {
+        UserGetRequest request = new UserGetRequest();
+        request.setWebRole(Sets.newHashSet(WebRole.ADMIN, WebRole.SITE_ADMIN));
+
+        for (User u : userProfileClient.getUsers(request)) {
+            if (u.getId() != notification.getReceiverId()) {
+                sendNotificationToUser(notification, u.getId());
+            }
+        }
+    }
+
+    /**
+     * Helper method that will send a web notification to the passed in user id.
+     * 
+     * @param notification The notification to be sent.
+     * @param userId       The user id of the user to send it too.
+     */
+    private void sendNotificationToUser(Notification notification, int userId) {
+        template.convertAndSend(String.format("%s/%s", NOTIFICATIONS, userId), notification);
     }
 }
