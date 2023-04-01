d7f89b86cb09c117f32e5260a31cfc38d5c14aed
SAM-LAPTOP\sambu <sambutler1017@icloud.com>
2023-04-01 16:56:23 -0400

    no message

--- src/test/java/com/marcs/app/auth/rest/AuthenticationControllerTest.java
+++ src/test/java/com/marcs/app/auth/rest/AuthenticationControllerTest.java
@@ -1,11 +1,7 @@
 package com.marcs.app.auth.rest;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
-
-import java.util.HashMap;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,7 +15,6 @@ import com.marcs.app.auth.client.domain.request.AuthenticationRequest;
 import com.marcs.app.auth.service.AuthenticationService;
 import com.marcs.app.featureaccess.client.FeatureAccessClient;
 import com.marcs.app.user.client.UserProfileClient;
-import com.marcs.app.user.client.domain.Application;
 import com.marcs.test.factory.abstracts.BaseControllerTest;
 import com.marcs.test.factory.annotations.MarcsRestTest;
 
@@ -43,19 +38,16 @@ public class AuthenticationControllerTest extends BaseControllerTest {
     private FeatureAccessClient featureAccessClient;
 
     @Test
-    public void testAuthenticate() throws Exception {
+    public void testAuthenticate() {
         when(service.authenticate(any(AuthenticationRequest.class))).thenReturn(new AuthToken());
-
-        AuthenticationRequest request = new AuthenticationRequest("test@mail.com", "testPassword");
-        check(post("/api/authenticate", request, AuthToken.class), serializedNonNull());
+        check(post("/api/authenticate", new AuthenticationRequest(), AuthToken.class),
+                serializedNonNull());
     }
 
     @Test
     @ControllerJwt
-    public void testReAuthenticate() throws Exception {
+    public void testReAuthenticate() {
         when(service.reauthenticate()).thenReturn(new AuthToken());
-        when(userProfileClient.getUserApps()).thenReturn(List.of(new Application()));
-        when(featureAccessClient.getFeatureAccess(anyInt())).thenReturn(new HashMap<>());
         check(post("/api/reauthenticate", AuthToken.class), serializedNonNull());
     }
 
@@ -63,5 +55,4 @@ public class AuthenticationControllerTest extends BaseControllerTest {
     public void testReAuthenticateNoToken() {
         check(post("/api/reauthenticate"), error(HttpStatus.UNAUTHORIZED, "Missing JWT Token."));
     }
-
 }
